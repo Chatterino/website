29782a005bca716836abd81e4fcff269e35175e6
pajlada <rasmus.karlsson@pajlada.com>
2022-03-31 14:16:31 +0200

    Fix Tailwind v3 deprecations (#23)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         gray: {
-          ...colors.trueGray,
+          ...colors.neutral,
           900: "#1A1919",
           800: "#252424",
           700: "#2F2D2D",

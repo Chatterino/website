c03c82b83386b811f92c720f6c1ec8fd7bea8839
Felanbird <41973452+Felanbird@users.noreply.github.com>
2022-10-28 17:36:45 -0400

    Drop scopes that have been removed by Twitch

--- pages/client_login.tsx
+++ pages/client_login.tsx
@@ -5,10 +5,6 @@ import Section from "../components/section";
 const redirectUrl = process.env.NEXT_PUBLIC_TWITCH_OAUTH_REDIRECT_URL!;
 const twitchClientID = process.env.NEXT_PUBLIC_TWITCH_OAUTH_CLIENT_ID!;
 const scopes = [
-  "user_subscriptions",
-  "user_blocks_edit", // deprecated, replaced with "user:manage:blocked_users"
-  "user_blocks_read", // deprecated, replaced with "user:read:blocked_users"
-  "user_follows_edit", // deprecated, soon to be removed later since we now use "user:edit:follows"
   "channel_editor", // for /raid
   "channel:moderate",
   "channel:read:redemptions",
@@ -18,7 +14,6 @@ const scopes = [
   "whispers:edit",
   "channel_commercial", // for /commercial
   "channel:edit:commercial", // in case twitch upgrades things in the future (and this scope is required)
-  "user:edit:follows", // for (un)following
   "clips:edit", // for clip creation
   "channel:manage:broadcast", // for creating stream markers with /marker command, and for the /settitle and /setgame commands
   "user:read:blocked_users", // for getting list of blocked users

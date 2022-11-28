dc5f9957d27acf177c62c24621c6072b30a8ab96
fourtf <tf.four@gmail.com>
2022-11-28 22:37:28 +0100

    bump for 2.4.0 (#128)
    * bump for 2.4.0
    
    * update changelog for 2.4.0

--- pages/download.tsx
+++ pages/download.tsx
@@ -41,7 +41,7 @@ function V1() {
       Legacy (really old + windows-only):{" "}
       {allV1Versions.reverse().map((v) => (
         <div>
-          <a href={`${baseDownloadLink}/1/chatterino-win-${v}.zip`}>{v}</a>
+          <a key={v} href={`${baseDownloadLink}/1/chatterino-win-${v}.zip`}>{v}</a>
         </div>
       ))}
     </div>

255ff143be025e6a8c8d989a3ac5d74d2001e5fa
Mateus Toledo <mateusmtoledo1@gmail.com>
2023-01-22 01:16:22 -0300

    Add xs screen breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xs: "475px",
+      ...defaultTheme.screens,
+    },
     extend: {
       colors: {
         gray: {

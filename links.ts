b7a57605ccfd52fc7395d5c97662d1a6f2c18d38
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2023-06-10 11:44:18 +0200

    Add Ubuntu deb links & move AppImage down

--- links.ts
+++ links.ts
@@ -9,7 +9,7 @@ export const nightly =
   "https://github.com/Chatterino/chatterino2/releases/tag/nightly-build";
 export const allDownloads = "download";
 
-const currentVersion = "2.4.4";
+export const currentVersion = "2.4.4";
 export const baseDownloadLink = `https://chatterino.fra1.digitaloceanspaces.com/bin`;
 const dl = `https://chatterino.fra1.digitaloceanspaces.com/bin/${currentVersion}`;
 

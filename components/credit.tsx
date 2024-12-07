c575d55f72227881181dcdacc5fa912cb6e009fa
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2024-12-07 22:18:50 +0100

    credit stuff

--- components/credit.tsx
+++ components/credit.tsx
@@ -1,9 +1,55 @@
 import React from "react";
 
-export default function FigureGroup({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div className="text-gray-300 mt-3 mb-4 text-sm">{children}</div>;
+type Props = {
+    author: string,
+    helper?: string,
+}
+
+function GitHubUser({ username }: { username: string }) {
+    // Authored by <a href="https://github.com/nerixyz" class="gh-author">
+    // <img src="https://avatars.githubusercontent.com/u/19953266?v=4&amp;s=60" class="gh-avatar">@nerixyz</a> with help from <a href="https://github.com/pajlada" class="gh-author"><img src="https://avatars.githubusercontent.com/u/962989?v=4&amp;s=60" class="gh-avatar">@pajlada</a>
+    const avatarURL = `https://avatars.githubusercontent.com/${username}?v=4&s=60`;
+    return (
+        <a href={`https://github.com/${username}`} className="gh-author"><img src={avatarURL} className="gh-avatar" />@{username}</a>
+    )
+}
+
+function formatUsers(users: string[] | undefined): React.ReactNode[] {
+    if (!users) {
+        return [];
+    }
+
+    return users.map(username => <GitHubUser username={username.trim()} />) || [<p>TODO REMOVE THIS SINCE AUTHORS SHOULD ALWAYS BE SET</p>];
+}
+
+export default function Credit({
+    author, helper
+}: Props) {
+    const formatList = (list: React.ReactNode[]) => {
+        if (list.length === 0) return null;
+        // @username
+        if (list.length === 1) return list[0];
+        // @username and @username
+        if (list.length === 2) return (<>{list[0]} and {list[1]}</>);
+
+        // @username, @username, and @username
+        return (
+            <>
+                {list.slice(0, -1).map((item, index) => (
+                    <span key={index}>{item}, </span>
+                ))}and {list[list.length - 1]}
+            </>
+        );
+    };
+
+    const authorElements = formatUsers(author.split(","));
+    const helperElements = formatUsers(helper?.split(","));
+
+    return <div className="text-gray-300 mt-3 mb-4 text-sm">
+        Authored by {formatList(authorElements)}
+        {
+            helperElements && (
+                <> with help from {formatList(helperElements)}</>)
+        }
+    </div>;
 }
--- next.config.mjs
+++ next.config.mjs
@@ -6,93 +6,50 @@ const baseRepoUrl = "https://github.com/Chatterino/Chatterino2";
 
 // -- #1234 issues
 function githubIssueLinks(options) {
-  return (tree) => {
-    findAndReplace(tree, [[/#\d+/g, replace]], {
-      ignore: ["link", "linkReference"],
-    });
-  };
-
-  function replace(value, _no, _match) {
-    return {
-      type: "link",
-      title: null,
-      url: `${baseRepoUrl}/issues/${value.slice(1)}`,
-      children: [{ type: "text", value }],
+    return (tree) => {
+        findAndReplace(tree, [[/#\d+/g, replace]], {
+            ignore: ["link", "linkReference"],
+        });
     };
-  }
-}
 
-const ignoredUsernames = new Set(["mentions", "usernames"]);
-
-function author(options) {
-  return (tree) => {
-    findAndReplace(tree, [[/@[\w\d-]+/g, replace]], {
-      ignore: ["link", "linkReference"],
-    });
-  };
-
-  function replace(value, _no, _match) {
-    const username = value.slice(1);
-    if (ignoredUsernames.has(username)) {
-      return _match;
+    function replace(value, _no, _match) {
+        return {
+            type: "link",
+            title: null,
+            url: `${baseRepoUrl}/issues/${value.slice(1)}`,
+            children: [{ type: "text", value }],
+        };
     }
-
-    const children = [
-      {
-        type: "image",
-        url: `https://avatars.githubusercontent.com/${encodeURIComponent(username)}?size=32`,
-        data: {
-          hProperties: {
-            className: "gh-avatar",
-            loading: "lazy",
-          },
-        },
-      },
-      { type: "text", value },
-    ];
-
-    return {
-      type: "link",
-      data: {
-        hProperties: {
-          className: "gh-author",
-        },
-      },
-      url: `https://github.com/${username}`,
-      children,
-    };
-  }
 }
 
 function majorMinorColoring() {
-  return (tree) => {
-    findAndReplace(tree, [[/Major:/g, replace]], {
-      ignore: ["link", "linkReference"],
-    });
-  };
-
-  function replace(value, _no, _match) {
-    return {
-      type: "strong",
-      children: [{ type: "text", value }],
+    return (tree) => {
+        findAndReplace(tree, [[/Major:/g, replace]], {
+            ignore: ["link", "linkReference"],
+        });
     };
-  }
+
+    function replace(value, _no, _match) {
+        return {
+            type: "strong",
+            children: [{ type: "text", value }],
+        };
+    }
 }
 
 // -- config
 const withMDX = (await import("@next/mdx")).default({
-  extension: /\.mdx$/,
-  options: {
-    remarkPlugins: [
-      githubIssueLinks,
-      author,
-      majorMinorColoring,
-      remarkHeadingId,
-    ],
-  },
+    extension: /\.mdx$/,
+    options: {
+        remarkPlugins: [
+            githubIssueLinks,
+            majorMinorColoring,
+            remarkHeadingId,
+        ],
+    },
 });
 
 export default withMDX({
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-  output: "export",
+    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+    output: "export",
 });

4f76161d4e7e928325fa23a9f48ba995564ef299
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2024-12-08 11:36:13 +0100

    run prettier

--- next.config.mjs
+++ next.config.mjs
@@ -6,50 +6,46 @@ const baseRepoUrl = "https://github.com/Chatterino/Chatterino2";
 
 // -- #1234 issues
 function githubIssueLinks(options) {
-    return (tree) => {
-        findAndReplace(tree, [[/#\d+/g, replace]], {
-            ignore: ["link", "linkReference"],
-        });
-    };
+  return (tree) => {
+    findAndReplace(tree, [[/#\d+/g, replace]], {
+      ignore: ["link", "linkReference"],
+    });
+  };
 
-    function replace(value, _no, _match) {
-        return {
-            type: "link",
-            title: null,
-            url: `${baseRepoUrl}/issues/${value.slice(1)}`,
-            children: [{ type: "text", value }],
-        };
-    }
+  function replace(value, _no, _match) {
+    return {
+      type: "link",
+      title: null,
+      url: `${baseRepoUrl}/issues/${value.slice(1)}`,
+      children: [{ type: "text", value }],
+    };
+  }
 }
 
 function majorMinorColoring() {
-    return (tree) => {
-        findAndReplace(tree, [[/Major:/g, replace]], {
-            ignore: ["link", "linkReference"],
-        });
-    };
+  return (tree) => {
+    findAndReplace(tree, [[/Major:/g, replace]], {
+      ignore: ["link", "linkReference"],
+    });
+  };
 
-    function replace(value, _no, _match) {
-        return {
-            type: "strong",
-            children: [{ type: "text", value }],
-        };
-    }
+  function replace(value, _no, _match) {
+    return {
+      type: "strong",
+      children: [{ type: "text", value }],
+    };
+  }
 }
 
 // -- config
 const withMDX = (await import("@next/mdx")).default({
-    extension: /\.mdx$/,
-    options: {
-        remarkPlugins: [
-            githubIssueLinks,
-            majorMinorColoring,
-            remarkHeadingId,
-        ],
-    },
+  extension: /\.mdx$/,
+  options: {
+    remarkPlugins: [githubIssueLinks, majorMinorColoring, remarkHeadingId],
+  },
 });
 
 export default withMDX({
-    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-    output: "export",
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  output: "export",
 });

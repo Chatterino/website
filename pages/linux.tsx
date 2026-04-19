08be708924312a7a57d5acc0f2ce1cde7d1afc96
killertofus <cinemassacres@gmail.com>
2026-04-19 00:35:49 -0600

    squashed commit

--- pages/linux.tsx
+++ pages/linux.tsx
@@ -3,16 +3,11 @@ import Section from "../components/section";
 import Link from "../components/link";
 import Text from "../components/text";
 import Page from "../components/page";
-import {
-  currentVersion,
-  linuxAppimageUrl,
-  linuxBuildFromSource,
-} from "../links";
+import { currentVersion, linuxBuildFromSource } from "../links";
 
 function LinuxPage() {
   const ubuntu2404Link = `https://github.com/Chatterino/chatterino2/releases/download/v${currentVersion}/Chatterino-Ubuntu-24.04.deb`;
   const ubuntu2204Link = `https://github.com/Chatterino/chatterino2/releases/download/v${currentVersion}/Chatterino-Ubuntu-22.04.deb`;
-  const ubuntu2004Link = `https://github.com/Chatterino/chatterino2/releases/download/v${currentVersion}/Chatterino-Ubuntu-20.04.deb`;
   return (
     <Page title="Chatterino on Linux">
       <div className="p-16">
@@ -40,16 +35,6 @@ function LinuxPage() {
             The Universe repository must be enabled for it to download the
             relevant dependencies.
           </Text>
-          {/* Ubuntu 20.04 */}
-          <h2 className="text-3xl pt-10 pb-4">Ubuntu 20.04 (Focal Fossa)</h2>
-          <Text>
-            A .deb file is available from{" "}
-            <Link href={ubuntu2004Link}>here</Link>.
-          </Text>
-          <Text>
-            The Universe repository must be enabled for it to download the
-            relevant dependencies.
-          </Text>
           {/* Flatpak */}
           <h2 className="text-3xl pt-10 pb-4">Flatpak</h2>
           <Text>
@@ -65,51 +50,81 @@ function LinuxPage() {
           {/* AUR */}
           <h2 className="text-3xl pt-10 pb-4">Arch Linux</h2>
           <Text>
-            Get the latest stable version with{" "}
+            You can install Chatterino through the{" "}
+            <Link href="https://wiki.archlinux.org/title/Arch_User_Repository">
+              Arch User Repository
+            </Link>{" "}
+            with{" "}
+            <Link href="https://wiki.archlinux.org/title/Makepkg">makepkg</Link>{" "}
+            or with an{" "}
+            <Link href="https://wiki.archlinux.org/title/AUR_helpers">
+              AUR helper
+            </Link>{" "}
+            like <Link href="https://github.com/Jguer/yay">yay</Link> or{" "}
+            <Link href="https://github.com/Morganamilo/paru">Paru</Link>.
+          </Text>
+          <br />
+          <Text>
+            Get the latest stable version from{" "}
             <Link href="https://aur.archlinux.org/packages/chatterino2-bin/">
               chatterino2-bin
-            </Link>{" "}
-            (e.g. with yay{" "}
-            <code className="p-1 px-2 mt-4 border-gray-600 border bg-gray-900">
-              yay -S chatterino2-bin
-            </code>
-            )
+            </Link>
+            <p className="pl-3 pb-3">
+              e.g.{" "}
+              <code className="p-1 px-2 mt-4 border-gray-600 border bg-gray-900">
+                yay -S chatterino2-bin
+              </code>{" "}
+              or{" "}
+              <code className="p-1 px-2 mt-4 border-gray-600 border bg-gray-900">
+                paru -S chatterino2-bin
+              </code>
+            </p>
           </Text>
           <Text>
-            Get the latest nightly version with{" "}
+            Get the latest nightly version from{" "}
             <Link href="https://aur.archlinux.org/packages/chatterino2-nightly-bin/">
               chatterino2-nightly-bin
-            </Link>{" "}
-            (e.g. with yay{" "}
-            <code className="p-1 px-2 mt-4 border-gray-600 border bg-gray-900">
-              yay -S chatterino2-nightly-bin
-            </code>
-            )
+            </Link>
+            <p className="pl-3 pb-3">
+              e.g.{" "}
+              <code className="p-1 px-2 mt-4 border-gray-600 border bg-gray-900">
+                yay -S chatterino2-nightly-bin
+              </code>{" "}
+              or{" "}
+              <code className="p-1 px-2 mt-4 border-gray-600 border bg-gray-900">
+                paru -S chatterino2-nightly-bin
+              </code>
+            </p>
           </Text>
           <Text>
-            Build latest stable version from source with{" "}
+            Build latest stable version from source from{" "}
             <Link href="https://aur.archlinux.org/packages/chatterino2/">
               chatterino2
-            </Link>{" "}
-            (e.g. with yay{" "}
-            <code className="p-1 px-2 mt-4 border-gray-600 border bg-gray-900">
-              yay -S chatterino2
-            </code>
-            )
+            </Link>
+            <p className="pl-3 pb-3">
+              e.g.{" "}
+              <code className="p-1 px-2 mt-4 border-gray-600 border bg-gray-900">
+                yay -S chatterino2
+              </code>{" "}
+              or{" "}
+              <code className="p-1 px-2 mt-4 border-gray-600 border bg-gray-900">
+                paru -S chatterino2
+              </code>
+            </p>
           </Text>
           <Text>
-            Build latest development version from source with{" "}
+            Build latest development version from source from{" "}
             <Link href="https://aur.archlinux.org/packages/chatterino2-git/">
               chatterino2-git
             </Link>{" "}
             (e.g. with yay{" "}
             <code className="p-1 px-2 mt-4 border-gray-600 border bg-gray-900">
               yay -S chatterino2-git
-            </code>
+            </code>{""}
             )
             </Text>
-          {/* Fedora */}
-          <h2 className="text-3xl pt-10 pb-4">Fedora Linux</h2>
+          {/* Fedora linux */}
+          <h2 className="text-3xl pt-10 pb-4">Fedora</h2>
           <Text>
             Get the latest stable version of Chatterino through{" "}
             <a href="https://src.fedoraproject.org/rpms/chatterino2">
@@ -127,35 +142,31 @@ function LinuxPage() {
           {/* OpenSuse */}
           <h2 className="text-3xl pt-10 pb-4">OpenSuse</h2>
           <Text>
-            Run{" "}
+            Get the latest stable version of Chatterino through{" "}
+            <a href="https://en.opensuse.org/SDB:OBS_Package_Installer">
+              OBS (Open Build Service)
+            </a>{" "}
             <code className="p-1 px-2 mt-4 border-gray-600 border bg-gray-900">
-              opi install chatterino2
-              </Text>
-            </code>{" "}
-            {" "}
-            Install Chatterino from
-            <Link href="https://en.opensuse.org/SDB:OBS_Package_Installer)">
-            OBS (Open Build Service)
+              sudo opi install chatterino2
+            </code>
+          </Text>
+          {/* Ubuntu 20.04 */}
+          <h2 className="text-3xl pt-10 pb-4">Ubuntu 20.04 (Focal Fossa)</h2>
+          <Text>
+            We no longer support Ubuntu 20.04 as of Chatterino v2.5.5. See{" "}
+            <a href="https://github.com/Chatterino/chatterino2/discussions/6522">
+              this discussion
+            </a>{" "}
+            for more information.
           </Text>
           {/* AppImage */}
-          <h2 className="text-3xl pt-10 pb-4">AppImage for all distros</h2>
+          <h2 className="text-3xl pt-10 pb-4">AppImage</h2>
           <Text>
-            You can install an AppImage with all dependencies bundled in a
-            single executable. Run this script in a terminal to install:
-            <div className="p-4 mt-4 bg-gray-900 border-gray-600 border">
-              <pre>cd /usr/local/bin</pre>
-              <pre>
-                sudo bash -c "curl {linuxAppimageUrl} &gt; ./chatterino"
-              </pre>
-              <pre>sudo chmod +x ./chatterino</pre>
-            </div>
-          </Text>
-          {/* Building from Source */}
-          <h2 className="text-3xl pt-10 pb-4">Building from Source</h2>
-          <Text>
-            If the options above don't work, then you can try{" "}
-            <Link href={linuxBuildFromSource}>this guide</Link> to build it from
-            source.
+            We no longer support AppImage as of Chatterino v2.5.5. See{" "}
+            <a href="https://github.com/Chatterino/chatterino2/discussions/6110">
+              this discussion
+            </a>{" "}
+            for more information.
           </Text>
         </Section>
       </div>

c2f3f9160c25a23b38dfdf571e2c7fc7744f4d0c
killertofus <cinemassacres@gmail.com>
2026-06-27 00:52:04 -0600

    fixed opensuse wording

--- pages/linux.tsx
+++ pages/linux.tsx
@@ -170,20 +170,16 @@ function LinuxPage() {
             </code>
           </Text>
 
-          {/* Fedora official */}
+          {/* Fedora Linux */}
           <h2 className="text-3xl pt-10 pb-4">Fedora Linux</h2>
           <Text>
             Get the latest stable version of Chatterino through{" "}
             <a href="https://src.fedoraproject.org/rpms/chatterino2">
               Fedora's official package
             </a>{" "}
-            packaged by{" "}
-            <a href="https://src.fedoraproject.org/user/solomoncyj">
-              solomoncyj
-            </a>
             :{" "}
             <code className="p-1 px-2 mt-4 border-gray-600 border bg-gray-900">
-              sudo dnf install chatterino
+             dnf install chatterino
             </code>
           </Text>
           {/* Building from Source */}
@@ -193,6 +189,33 @@ function LinuxPage() {
             <Link href={linuxBuildFromSource}>this guide</Link> to build it from
             source.
           </Text>
+
+{/* openSUSE */}
+          <h2 className="text-3xl pt-10 pb-4">openSUSE</h2>
+          <Text>
+            Get the latest stable version of Chatterino through{" "}
+            <a href="https://en.opensuse.org/SDB:OBS_Package_Installer">
+              OBS (Open Build Service)
+            
+            
+            </a>
+            :{" "}
+                 
+            
+            
+            <code className="p-1 px-2 mt-4 border-gray-600 border bg-gray-900">
+             opi install chatterino
+            </code>
+          </Text>
+          {/* Building from Source */}
+          <h2 className="text-3xl pt-10 pb-4">Building from Source</h2>
+          <Text>
+            If the options above don't work, then you can try{" "}
+            <Link href={linuxBuildFromSource}>this guide</Link> to build it from
+            source.
+          </Text>
+
+
           {/* Ubuntu 20.04 */}
           <h2 className="text-3xl pt-10 pb-4">Ubuntu 20.04 (Focal Fossa)</h2>
           <Text>

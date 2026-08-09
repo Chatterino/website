5e94415d527021621012b521d4f22785732cad7a
jprochazk <pjanv42@gmail.com>
2026-08-09 17:32:12 +0200

    delet

--- components/figure-group.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function FigureGroup({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div className="figure-group">{children}</div>;
-}
